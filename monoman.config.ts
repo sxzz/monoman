36b98cc0479602d79857bd154cc67b855e100ab3
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-02-17 14:59:57 +0100

    chore: remove test demo

--- monoman.config.ts
+++ monoman.config.ts
@@ -2,14 +2,6 @@ import { defineConfig } from './src/index'
 
 export default defineConfig([
   {
-    include: 'LICENSE',
-    exclude: [],
-    type: 'text',
-    write(text) {
-      return `${text}LICENSE`
-    },
-  },
-  {
     include: 'package.json',
     type: 'json',
     write(data: any) {
