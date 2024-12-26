84b13c22123269ca665f79046553e2fd2c3ecd6e
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-12-26 21:44:45 +0800

    chore: bundling by tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  format: ['esm'],
+  format: 'esm',
   target: 'node18.12',
   clean: true,
   dts: true,
