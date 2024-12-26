c4bdf6131c7341de03f8d404e423172890da07b4
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-12-26 21:38:25 +0800

    feat!: drop cjs support

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   target: 'node18.12',
   clean: true,
   dts: true,
