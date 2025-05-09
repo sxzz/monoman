f4d2d8ac6f0c4558b7045e54e94337473dbae323
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-09 08:59:13 +0800

    feat!: drop node 18

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  format: 'esm',
-  target: 'node18.12',
-  clean: true,
-  dts: true,
-  bundleDts: { resolve: ['@antfu/utils'] },
-  shims: true,
+  entry: './src/index.ts',
+  dts: { resolve: ['@antfu/utils'] },
 })
